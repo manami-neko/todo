950d62f807b90a8c82c2b73caede163b5da6d23d
manami <utau.3152@outlook.>
2025-01-29 22:24:18 +0900

    Todoアプリ3-4

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoController;
+use App\Http\Controllers\CategoryController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,9 +19,8 @@ Route::get('/', function () {
 });
 
 Route::get('/',[TodoController::class, 'index']);
-
 Route::post('/todos',[TodoController::class,'store']);
-
 Route::patch('/todos/update', [TodoController::class, 'update']);
-
 Route::delete('/todos/delete', [TodoController::class, 'destroy']);
+
+Route::get('/categories', [CategoryController::class, 'index']);
